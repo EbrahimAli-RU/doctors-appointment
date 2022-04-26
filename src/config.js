d9cfed2a9f5b028faf3ba45cc12417852a6d0df0
Ebrahim <ebrahimali.cse.ru@gmail.com>
2022-04-26 12:53:13 +0600

    setup basic folder structure and install needed dependencies

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const CLIENT_ID =
+    "1042674076178-d7gmdlr9k6q0rf659r1vaghb646if0gc.apps.googleusercontent.com",
+  GOOGLE_API_KEY = "AIzaSyDYwxwDmfnJhBQGw3ZBhKKxQ_KthS32fBE",
+  DISCOVERY_DOCS = [
+    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  ],
+  CALENDAR_ID = "2j4snt99vsje1ir0ljsr6o8j9k@group.calendar.google.com",
+  SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
